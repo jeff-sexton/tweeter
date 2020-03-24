be364495d57f84fbdb893cf6146cb34d3e5e11bf
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-24 11:29:21 -0700

    renamed jquery object variables

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,11 +5,11 @@ $(document).ready(() => {
     let charLeft = 140 - $(this).val().length;
 
     /* Update Couter Value */
-    let counter = $(this).next('footer').children('.counter');
-    counter.val(charLeft);
+    let $counter = $(this).next('footer').children('.counter');
+    $counter.val(charLeft);
 
     /* Toggle .red class when too many characters present */
-    $(counter).toggleClass('red', charLeft < 0);
+    $($counter).toggleClass('red', charLeft < 0);
 
   });
 
