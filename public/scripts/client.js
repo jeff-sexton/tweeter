f4bb1bbcf171535784c0dc50ca09b65b75c482ce
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-24 12:53:39 -0700

    Created renderTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,38 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const exampleTweet =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
 
 $(document).ready(()=> {
-
-  const $tweet = createTweetElement(exampleTweet);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweet-display').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
 });
 
 
-const createTweetElement = function(tweetObj) {
+const createTweetElement = (tweetObj) => {
   const $newTweet = $('<article>').addClass('tweet');
   
   $newTweet.html(
@@ -47,6 +55,14 @@ const createTweetElement = function(tweetObj) {
   );
 
   return $newTweet;
+
+};
+
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    const $newTweet = createTweetElement(tweet);
+    $('.tweet-display').append($newTweet);
+  }
 
 };
 
