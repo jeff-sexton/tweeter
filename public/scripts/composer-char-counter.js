21ea933b5198eaede89970905b70345dd5f546c9
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-25 19:15:01 -0700

    Cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,17 @@
+/*eslint-env jquery*/
+/* global document */
+
 $(document).ready(() => {
  
   // Origionally keyup; changed to input to capture mouse paste/copy events;
    
   $('.new-tweet #tweet-text').on('input', function() {
-    /* Calculate characters left from max of 140 */
     let charLeft = 140 - $(this).val().length;
 
-    /* Update Couter Value */
     let $counter = $(this).next('.new-tweet-footer').children('.counter');
     $counter.val(charLeft);
 
-    /* Toggle .red class when too many characters present */
     $($counter).toggleClass('red', charLeft < 0);
-
   });
 
 
